9ef131c95d8e99d0d9fbfa365bdf4e67d26ec699
Radiojitter Concepts Lab LLP <help@radiojitter.com>
2018-10-05 15:46:59 +0530

    Add files via upload

---
+++ Realtime_RTLSDR_VOR/VORViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VORViewer")]
+[assembly: AssemblyDescription("GUI for VOR Data Display")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Arnav Mukhopadhyay")]
+[assembly: AssemblyProduct("VORViewer")]
+[assembly: AssemblyCopyright("Copyright Arnav Mukhopadhyay @ RadioJitter ©  2018")]
+[assembly: AssemblyTrademark("VORVIEWER Copyright Arnav Mukhopadhyay @ RadioJitter ©  2018")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2eb2816-6403-4bdd-960f-b0f1bbe19f64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
